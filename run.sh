e75afc5d2ee5efc9ede18f907ff6115d85a0059a
Artur Belousov <g.infosecurity@gmail.com>
2015-04-27 12:05:12 +0300

    edit universal run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
-export JAVA_HOME="/usr/lib/jvm/jdk1.7.0_40"
-java -jar artifactid-1.0-SNAPSHOT.jar $1
-google-chrome /tmp/index.html
+#!/bin/bash
+find /usr/lib/jvm/ -name "jdk1.7*" |while read line; 
+do
+     STR="export JAVA_HOME=$line"
+     echo $STR > source.sh
+done
+source source.sh
+rm -f source.sh
+echo $JAVA_HOME
+$JAVA_HOME/bin/java -jar target/artifactid-1.0-SNAPSHOT.jar $1
+firefox /tmp/index.html
